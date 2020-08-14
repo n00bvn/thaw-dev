4d26d69093eab31262a3dc5a2a83002e72e36bdd
Tho Tran <tho.tran1809@gmail.com>
2020-08-15 02:35:19 +0700

    added skype contact

--- src/components/About.js
+++ src/components/About.js
@@ -21,10 +21,10 @@ export default function About() {
           <div className="animate__animated animate__bounceInDown">
             <h1 className="mb-0">
               <span className="text-primary">Thaw</span>&nbsp;Tran
-          </h1>
+            </h1>
             <div className="subheading mb-5">
               Full stack developer | Ruby, Python, React, Meteor
-          </div>
+            </div>
           </div>
         )}
         <div className="lead mb-5" id="IntroContent">
@@ -35,6 +35,7 @@ export default function About() {
             <a className="social-icon" href="mailto:hi@thaw.dev"><i className="fas fa-envelope"></i></a>
             <a className="social-icon" href="https://github.com/n00bvn" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
             <a className="social-icon" href="https://thaw.dev/CV_Thaw.pdf" target="_blank" rel="noopener noreferrer"><i className="fas fa-file"></i></a>
+            <a href="https://join.skype.com/invite/Vc7F9bjA1aFv" className="social-icon" target="_blank" rel="noopener noreferrer"><i className="fab fa-skype"></i></a>
           </div>
         )}
       </div>
