3f13c8d0c93b75a16d29141c61a81bade24871da
Tho Tran <tho.tran1809@gmail.com>
2024-06-23 23:51:52 +0700

    Removed apikey of tinymce editor

--- src/components/admin/tinymce_editor.js
+++ src/components/admin/tinymce_editor.js
@@ -61,7 +61,6 @@ export default function TinymceEditor({ initValue, handleEditorChange }) {
 
   return (
     <Editor
-      apiKey='tzlqdv32xyexkcxj8vyngahaf5uboud6x7b8uh87vjxv0czv'
       initialValue={initValue}
       init={INIT_TINYMCE_EDITOR}
       onEditorChange={handleEditorChange}
